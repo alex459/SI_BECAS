6df9b62c10f6cba04d3baa8daf1070999006d51a
MauricioBC <MauricioBC@MauricioBC-PC>
2016-11-20 15:24:41 -0600

    corregido nombre de anotacion de servlet eliminar oferta de beca

--- src/java/MODEL/EliminarOfertaBecaServlet.java
+++ src/java/MODEL/EliminarOfertaBecaServlet.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author MauricioBC
  */
-@WebServlet("/ModificarOfertaBecaServlet")
+@WebServlet("/EliminarOfertaBecaServlet")
 public class EliminarOfertaBecaServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
