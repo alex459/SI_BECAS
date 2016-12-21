d218701a078f23f7bfe4fffeaae67862aab4c8a0
alex <alex@alexito>
2016-12-21 15:58:06 -0600

    resolver solicitud:pantallas fiscalia

---
+++ web/js/resolverSolFiscaliaActaReintegro.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+angular.module('resolverSolFiscaliaActaReintegroApp', []).controller('resolverSolFiscaliaActaReintegroCtrl', function ($scope) {
+    
+$scope.idTipo;
+$scope.observacion;
+$scope.resolucion="NO SE HA SELECCIONADO UNA OPCION";
+$scope.CambiarEstadoAprobado = function (){
+    $scope.resolucion = $scope.resolucion="APROBADO";
+};
+$scope.CambiarEstadoDenegado = function (){
+    $scope.resolucion = $scope.resolucion="DENEGADO";
+};
+$scope.CambiarEstadoCorreccion = function (){
+    $scope.resolucion = $scope.resolucion="CORRECCION";
+};
+  });
